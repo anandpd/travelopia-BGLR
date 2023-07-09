75763ee17f9bed78f92a26ff2fc6e426f1518a05
anand <anand.prasad@appinventiv.com>
2023-07-10 00:59:04 +0530

    updated to all hosts

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,9 +12,9 @@ const app: Application = express();
 
 morganBody(app);
 app.use(express.json());
-app.use(cors({origin: "*"}));
+// app.use(cors({origin: "*"}));
 const corsMiddleware = function(req:Request, res:Response, next:NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'localhost'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Origin', '*'); //replace localhost with actual host
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     next();
