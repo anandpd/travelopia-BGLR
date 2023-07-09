5014291ca3357fcb21a2bf48d709df96bf69db43
anand <anand.prasad@appinventiv.com>
2023-07-09 22:46:36 +0530

    code cleanup

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
 import "./app.style.css";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { AdminDash, AdminListing, Header, Home } from "./components";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,7 +7,6 @@ import { RouteNotFound } from "./components/RouteNotFound/RouteNotFound";
 import { Protected } from "./components/Protected/ProtectedComponent";
 
 function App() {
-  const location = useLocation();
   return (
     <>
       <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
