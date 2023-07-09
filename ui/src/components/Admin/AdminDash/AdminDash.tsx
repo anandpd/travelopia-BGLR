7242c798ee5cbf73eaa5f9cd4d648e0ff3759253
anand <anand.prasad@appinventiv.com>
2023-07-09 16:47:40 +0530

    not found route addded

---
+++ ui/src/components/Admin/AdminDash/AdminDash.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import './admindash.style.css';
+
+export const AdminDash:React.FC<any> = (): ReactElement => {
+    return (
+        <h2>
+            Admin
+        </h2>
+    )
+}
---
+++ ui/src/components/RouteNotFound/RouteNotFound.tsx
@@ -0,0 +1,19 @@
+import React, { ReactElement } from "react";
+import { Container, Image, Row } from "react-bootstrap";
+import "./routenotfound.style.css";
+import { Link } from "react-router-dom";
+
+export const RouteNotFound: React.FC<any> = (): ReactElement => {
+  return (
+    // <div className="txt-404">
+      <Container className="container-404">
+        <div>
+          <h2> Let's Get Lost !!  but not here ...</h2>
+        </div>
+        <div>
+              Click on  <Link to="/"> Home </Link> to get back
+        </div>
+      </Container>
+    // </div>
+  );
+};
