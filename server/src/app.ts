fd2fbc41083a479d0168eb557c18d1461699aa89
anand <anand.prasad@appinventiv.com>
2023-07-10 11:13:21 +0530

    cors update

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,14 +12,14 @@ const app: Application = express();
 
 morganBody(app);
 app.use(express.json());
-app.use(cors({ allowedHeaders: "*", origin: "*", methods: "*" }));
-// const corsMiddleware = function(req:Request, res:Response, next:NextFunction) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', '*');
-//     res.header('Access-Control-Allow-Headers', '*');
-//     next();
-// }
-// app.use(corsMiddleware);
+// app.use(cors({ allowedHeaders: "*", origin: "*", methods: "*" }));
+const allowCrossDomain = (req:Request, res:Response, next:NextFunction) => {
+    res.header(`Access-Control-Allow-Origin`, `https://travelopia-backend-node.vercel.app`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+  };
+app.use(allowCrossDomain);
 app.get('/', async (req, res) => res.send("<p>Travelopia Server</p>"))
 app.use("/v1", routes);
 process.on('unhandledRejection', (e: Error) => {
