5d223847dd30600d0ef83b3df1dd757ebd7ba2ad
anand <anand.prasad@appinventiv.com>
2023-07-10 23:32:58 +0530

    for deployment

--- ui/src/mutations/getAdminQueries.ts
+++ ui/src/mutations/getAdminQueries.ts
@@ -5,7 +5,7 @@ export async function getAdminQueriesMutation(): Promise<any> {
     try {
         const responseData = await axios({
             method: 'get',
-            headers: { 'isadmin': "true",  "Content-Type": "application/json" },
+            headers: { 'isadmin': "true", "Content-Type": "application/json" },
             url: 'https://travelopia-backend-node.onrender.com/v1/admin/queries',
         });
         return responseData;
