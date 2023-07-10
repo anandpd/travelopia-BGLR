526bf1642e7f0937d998535923e37fc843e2df23
anand <anand.prasad@appinventiv.com>
2023-07-10 11:47:30 +0530

    validation commented for debug

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -5,8 +5,13 @@ import { AdminSignIn } from '../validations';
 import { AdminDeleteQuery } from '../validations/admin.validation';
 const router = Router();
 
-router.post('/signin', SchemaValidator([{ schema: AdminSignIn(), on: 'body' }]), ValidateAdmin, AdminController.SignIn);
+router.post('/signin', 
+// SchemaValidator([{ schema: AdminSignIn(), on: 'body' }]), 
+ValidateAdmin, AdminController.SignIn);
+
 router.get('/queries', ValidateAdmin, AdminController.GetAllQueries);
-router.delete("/query/:id", ValidateAdmin, SchemaValidator([{ schema: AdminDeleteQuery(), on: "params" }]), AdminController.DeleteQuery);
+router.delete("/query/:id", ValidateAdmin,
+//  SchemaValidator([{ schema: AdminDeleteQuery(), on: "params" }]), 
+ AdminController.DeleteQuery);
 
 export default router;
